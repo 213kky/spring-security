cf559162195fa1de0517ab6828afc8934848aa94
213kky <213kky@naver.com>
2025-01-05 20:26:25 +0900

    feat: JwtAuthenticationProvider 추가

--- src/main/java/com/example/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/springsecurity/config/SecurityConfig.java
@@ -1,11 +1,17 @@
 package com.example.springsecurity.config;
 
 import com.example.springsecurity.security.JwtAuthenticationFilter;
+import com.example.springsecurity.security.CustomMemberDetailsService;
+import com.example.springsecurity.security.JwtAuthenticationProvider;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -21,6 +27,8 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomMemberDetailsService customMemberDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -50,6 +58,14 @@ public class SecurityConfig {
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean // 다중 AuthenticationProvider 등록가능
+    public AuthenticationManager authenticationManager() throws Exception {
+        List<AuthenticationProvider> authenticationProviders = new ArrayList<>();
+        authenticationProviders.add(new JwtAuthenticationProvider(customMemberDetailsService, passwordEncoder()));
+
+        return new ProviderManager(authenticationProviders);
     }
 
     @Bean
