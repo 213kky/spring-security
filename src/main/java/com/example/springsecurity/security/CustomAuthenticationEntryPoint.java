d90a85ffdba56fe5de21a0bc0ec8d3d7cb6396d7
213kky <213kky@naver.com>
2025-01-12 23:28:49 +0900

    refactor: CustomAuthenticationEntryPoint JSON 응답 방식으로 변경
    - 기존 sendError 방식에서 JSON 응답 방식으로 변경
    - sendError 방식은 추가적으로 /error 컨트롤러를 호출해 리소스 소모가 더 크고 불필요하다 판단하여 응답 방식을 수정하였습니다.
    - 일관된 에러 응답 구조 제공

--- src/main/java/com/example/springsecurity/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/example/springsecurity/security/CustomAuthenticationEntryPoint.java
@@ -1,9 +1,11 @@
 package com.example.springsecurity.security;
 
+import com.example.springsecurity.util.ResponseUtil;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
@@ -12,6 +14,6 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
                         AuthenticationException authException) throws IOException, ServletException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+        ResponseUtil.createResponseBody(response, authException.getMessage(), HttpStatus.UNAUTHORIZED);
     }
 }
