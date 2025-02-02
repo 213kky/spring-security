9ded101b64980786ad5f2047f1e96b544f5bbe3e
213kky <213kky@naver.com>
2025-02-02 21:34:40 +0900

    fix: 회원가입 시 비밀번호를 BCrypt로 암호화하도록 수정

--- src/main/java/com/example/springsecurity/service/MemberService.java
+++ src/main/java/com/example/springsecurity/service/MemberService.java
@@ -1,8 +1,10 @@
 package com.example.springsecurity.service;
 
+import com.example.springsecurity.domain.Member;
 import com.example.springsecurity.dto.SignUpRequest;
 import com.example.springsecurity.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,9 +12,20 @@ import org.springframework.stereotype.Service;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     public void signUp(SignUpRequest request) {
-        memberRepository.save(request.toMember());
+        String encodedPassword = passwordEncoder.encode(request.password());
+        Member member = Member.builder()
+                .email(request.email())
+                .password(encodedPassword)
+                .name(request.name())
+                .role(request.role())
+                .provider(request.provider())
+                .nickName(request.nickName())
+                .build();
+
+        memberRepository.save(member);
     }
 
 }
